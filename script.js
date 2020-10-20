5f100e8fff5ef72dfd73f42206d627edfe7b0045
George <georgiyshatskiy@gmail.com>
2020-10-20 15:04:17 +0500

    js-window

---
+++ script.js
@@ -0,0 +1,21 @@
+var name = prompt("Введите имя")
+var age = prompt("Ваш возраст?")
+var ex1 = prompt("Решите пример №1:  3 + 9 ")
+var ex2 = prompt("Решите пример №2:  9 - 7 ")
+var ex3 = prompt("Решите пример №3:  6 * 8 ")
+var ex4 = prompt("Решите пример №4:  55 / 5 ")
+var ex5 = prompt("Решите пример №5:  55 % 12 ")
+var pdsk = alert("Откройте консоль: Ctrl + Shift + I, Вкладка 'Console'")
+var okansw = (" Правильный ответ: ")
+
+
+console.log("Имя: " +name)
+console.log("Возраст: " +age)
+console.log("Пример №1: ваш ответ: " +ex1 + ";" + okansw + 12+";")
+console.log("Пример №2: ваш ответ: " +ex2 + ";" + okansw + 2+";")
+console.log("Пример №3: ваш ответ: " +ex3 + ";" + okansw + 48+";")
+console.log("Пример №4: ваш ответ: " +ex4 + ";" + okansw + 11+";")
+console.log("Пример №5: ваш ответ: " +ex5 + ";" + okansw + 7+";")
+
+
+
